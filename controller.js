afef3ce180013b36ad6415dff4b32448809a8be0
Elena Payán <elena.payan@kairosds.com>
2020-04-16 22:45:29 +0200

    WIP

--- controller.js
+++ controller.js
@@ -11,8 +11,21 @@ let Post;
 
 app.get("/posts", (req, res) => {
     res.status(200).send({ posts: [] });
-})
+});
 
+app.get("/posts/postId", (req, res) => {
+});
+
+app.post("/posts", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ message: "encontrado" });
+});
+
+app.put("/posts/postId", (req, res) => {
+});
+
+app.delete("/posts/postId", (req, res) => {
+});
 
 
 async function dbConnect() {
@@ -26,14 +39,18 @@ async function dbConnect() {
     console.log("Connected to Mongo");
 
     var postSchema = new mongoose.Schema({
-        description: String,
+        autor: String,
+        nickname: String,
+        titulo: String,
+        texto: String,
+        comentarios: String
     });
 
     Post = mongoose.model("Post", postSchema);
 }
 
 async function main() {
-    
+
     await dbConnect();
 
     app.listen(3000, () => console.log('Server started in port 3000'));
