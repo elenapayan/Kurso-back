b25c4295323ae6ddab87fe8a8ac985b6147d920c
Elena Payán <elena.payan@kairosds.com>
2020-04-17 00:33:52 +0200

    WIP

--- controller.js
+++ controller.js
@@ -5,26 +5,77 @@ const url = "mongodb://admin:admin@localhost:27018/blogDB?authSource=admin";
 
 let Post;
 
-// app.post("/posts", async(req,res)=>{
+app.post("/posts", async (req, res) => {
+    console.log(req.body);
+    const post = new Post({
+        autor: req.body.autor,
+        nickname: req.body.nickname,
+        titulo: req.body.titulo,
+        texto: req.body.texto,
+        comentarios: req.body.comentarios
+    })
+    await post.save((err, postSaved) => {
+        if (err) {
+            res.status(500).send({ message: `Error al guardar en la base de datos:${err}` });
+        } else {
+            res.status(200).send({ post: postSaved });
+        }
+    });
+});
 
-// })
-
-app.get("/posts", (req, res) => {
+app.get("/posts", async (req, res) => {
+    const post = await Post.find();
+    if (!post) {
+        res.status(404).send({ message: "No encontrado" });
+    } else {
+        res.status(200).send({ post });
+        // res.json(post);
+    }
     res.status(200).send({ posts: [] });
 });
 
-app.get("/posts/postId", (req, res) => {
+app.get("/posts/:postId", async (req, res) => {
+    const postId = req.params.postId;
+    const post = await Post.findById(postId);
+    if (!post) {
+        res.status(404).send({ message: "No encontrado" });
+    } else {
+        res.status(200).send({ post });
+        // res.json(post);
+    }
 });
 
-app.post("/posts", (req, res) => {
-    console.log(req.body);
-    res.status(200).send({ message: "encontrado" });
+app.put("/posts/:postId", async (req, res) => {
+    const postId = req.params.postId;
+    const post = await Post.findById(postId);
+    if (!post) {
+        res.status(404).send({ message: "No encontrado" });
+    } else {
+        post.autor = req.body.autor;
+        post.nickname = req.body.nickname;
+        post.titulo = req.body.titulo;
+        post.texto = req.body.texto;
+        post.comentarios = req.body.comentarios;
+        await post.save((err, post) => {
+            if (err) {
+                res.status(500).send({ message: `Error al actualizar en la base de datos:${err}` });
+            } else {
+                res.status(200).send({ post });
+            }
+        });
+    }
 });
 
-app.put("/posts/postId", (req, res) => {
-});
-
-app.delete("/posts/postId", (req, res) => {
+app.delete("/posts/:postId", async (req, res) => {
+    const postId = req.params.postId;
+    const post = await Post.findById(postId);
+    if (!post) {
+        res.status(404).send({ message: "No encontrado" });
+    } else {
+        await Post.findByIdAndDelete(postId);
+        res.status(200).send({ post });
+        // res.json(post);
+    }
 });
 
 
@@ -38,7 +89,7 @@ async function dbConnect() {
 
     console.log("Connected to Mongo");
 
-    var postSchema = new mongoose.Schema({
+    const postSchema = new mongoose.Schema({
         autor: String,
         nickname: String,
         titulo: String,
