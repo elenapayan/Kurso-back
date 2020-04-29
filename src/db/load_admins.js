bcad41b6ef2d8a4670b83b9df9fcf62512e9c6db
Elena Payán <elena.payan@kairosds.com>
2020-04-29 12:44:19 +0200

    Add admins by json

--- src/db/load_admins.js
+++ src/db/load_admins.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const UsersRepository = require("../repositories/user");
+const admins = require("./admins.json");
 
 class PopulateUsers {
     constructor() { }
@@ -9,18 +10,21 @@ class PopulateUsers {
         try {
             const users = await UsersRepository.getUsers();
             if (users.length === 0) {
-                const user1 = { username: 'admin1', password: "1234", role: "admin" };
-                const user2 = { username: 'admin2', password: "1234", role: "admin" };
-                UsersRepository.saveUser(user1.username, user1.password, user1.role);
-                UsersRepository.saveUser(user2.username, user2.password, user2.role);
+                // UsersRepository.saveUser(admins.admin1.username, admins.admin1.password, admins.admin1.role);
+                // UsersRepository.saveUser(admins.admin2.username, admins.admin2.password, admins.admin2.role);
+                // UsersRepository.saveUser(admins.admin3.username, admins.admin3.password, admins.admin3.role);
+                const createAdmins = Object.entries(admins)
+                    .map(admin => Object.values(admin[1]));
+                // console.log("createAdmins", createAdmins);
+                createAdmins.map(admin => UsersRepository.saveUser(admin[0], admin[1], admin[2]));
                 console.info('Populate users success');
             } else {
-                console.log("LengthUsers es distinto de 0")
+                console.log("LengthUsers es distinto de 0");
             }
         } catch (err) {
             console.log(err);
         }
-    }
+    };
 }
 
 module.exports = new PopulateUsers();
